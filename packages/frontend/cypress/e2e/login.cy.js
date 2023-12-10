40c3f487d58d0d846f6c9c7c8cad41ef7dab1a98
HedonisticOpportunist <fujitahibaraki@gmail.com>
2023-12-10 20:14:17 +0000

    Update login.cy.js

--- packages/frontend/cypress/e2e/login.cy.js
+++ packages/frontend/cypress/e2e/login.cy.js
@@ -1,6 +1,6 @@
 describe("login spec", () => {
   it("allows the user to login with an email and a password", () => {
-    cy.visit("http://localhost:3000/login");
+    cy.visit("https://space-cats.vercel.app/login");
     cy.get('input[type="email"]').type("test@test.com");
     cy.get('input[type="password"]').type("test1234");
     cy.get("button").contains("Login").click();
