a3cf2618233053256873b883dcf3b2e941ac415e
HedonisticOpportunist <fujitahibaraki@gmail.com>
2023-12-10 20:15:09 +0000

    Update signup.cy.js

--- packages/frontend/cypress/e2e/signup.cy.js
+++ packages/frontend/cypress/e2e/signup.cy.js
@@ -1,6 +1,6 @@
 describe("signup spec", () => {
   it("allows the user to sign up with a username, email and password", () => {
-    cy.visit("http://localhost:3000/signup");
+    cy.visit("https://space-cats.vercel.app/signup");
     cy.get('input[type="username"]').type("testUser");
     cy.get('input[type="email"]').type("test@test.com");
     cy.get('input[type="password"]').type("test1234");
